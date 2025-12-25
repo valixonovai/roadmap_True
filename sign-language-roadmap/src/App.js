7672e35fab67bfd97b77e1992f561ab5bb2aa1ba
valixonovai <talaba4077@gmail.com>
2025-12-25 17:11:19 +0500

    Initial commit

---
+++ sign-language-roadmap/src/App.js
@@ -0,0 +1,12 @@
+// App.js (artifact code placeholder)
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Sign Language Roadmap</h1>
+    </div>
+  );
+}
+
+export default App;
